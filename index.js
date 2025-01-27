79fbdcea9336c407949bb35730d3077b8b91bc14
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-01-27 14:30:32 +0530

    updated code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
+      "http://localhost:5173",
       "https://assignment11-client-c377c.web.app",
     ],
     credentials: true,
